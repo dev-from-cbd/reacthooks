81e86c2a443b525b0a19d559d81c0847b6dd22fd
dev-from-cbd <veryfatwombat@gmail.com>
2021-12-25 13:20:56 +1100

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
+import trashIcon from "./trash.svg";
+
+function ListItem(props) {
+  return (
+    <div className="list-item row jc-space-between">
+      <span>List Item</span>
+
+      <img className="delete-icon" src={trashIcon} />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="app-background">
-      <p>Planing app</p>
+      <p className="heading-text">Planing Management App</p>
+
+      <div className="task-container">
+        <div>
+          <input className="text-input" placeholder="Write a task" />
+          <button className="add-button" type="submit">
+            Add
+          </button>
+        </div>
+
+        <p className="no-item-text">No tasks added.</p>
+
+        <ListItem />
+      </div>
     </div>
   );
 }
