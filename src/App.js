6f3e834050896eaa21ba1119f866260266030dab
dev-from-cbd <veryfatwombat@gmail.com>
2021-12-27 09:40:52 +1100

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import trashIcon from "./trash.svg";
 
-function ListItem(props) {
-  return (
-    <div className="list-item row jc-space-between">
-      <span>List Item</span>
-
-      <img className="delete-icon" src={trashIcon} />
-    </div>
-  );
-}
-
 function App() {
+  const [taskInput, updateTaskInput] = useState("");
+
   return (
     <div className="app-background">
       <p className="heading-text">Planing Management App</p>
 
       <div className="task-container">
         <div>
-          <input className="text-input" placeholder="Write a task" />
+          <input
+            className="text-input"
+            placeholder="Write a task"
+            value={taskInput}
+            onChange={updateTaskInput}
+          />
           <button className="add-button" type="submit">
             Add
           </button>
@@ -30,6 +27,16 @@ function App() {
       </div>
 
       <p className="footer-text">Developed by Oleg Gribanov</p>
+    </div>
+  );
+}
+
+function ListItem(props) {
+  return (
+    <div className="list-item row jc-space-between">
+      <span>List Item</span>
+
+      <img className="delete-icon" src={trashIcon} />
     </div>
   );
 }
